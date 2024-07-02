e59873756028a23d11841af5a2496638f57cc96b
Olubunmi <bjemiyor@gmail.com>
2024-07-02 09:35:33 +0100

    clean up app.js and format README.md

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 
 - **Endpoint: [GET] <example.com>/api/hello?visitor_name="Mark" (where <example.com> is your server origin)
 
-Response:
+## Response:
+
+```json
 {
   "client_ip": "127.0.0.1", // The IP address of the requester
-  "location": "New York" // The city of the requester
+  "location": "New York", // The city of the requester
   "greeting": "Hello, Mark!, the temperature is 11 degrees Celcius in New York"
 }
--- app.js
+++ app.js
@@ -12,33 +12,28 @@ app.use(express.json());
 
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Guest';
-    let clientIp = req.ip;  // This gives you the IP address of the requester
+    // let clientIp = req.ip;  // This gives you the IP address of the requester
     
     const accessToken = process.env.ACCESS_TOKEN;
 
     // Example of checking X-Forwarded-For header for client IP address
     let clientIpp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(clientIpp)
-
-    
-    
-    // console.log('access: ', accessToken)
+    // console.log(clientIpp)
 
     // Normalize IPv4-mapped IPv6 address to IPv4 format (if needed)
+    /*
     if (clientIp.startsWith('::ffff:')) {
         clientIp = clientIp.substring(7); // Remove the '::ffff:' prefix
     }
+    */
 
     try {
         // Fetch geolocation data based on IP address from ipinfo.io
 
         const getCity = await axios.get(`http://ip-api.com/json/${clientIpp}?fields=61439`)
-        console.log(getCity)
-        
-
         // console.log(getCity)
 
-        const { city, country } = getCity.data;
+        const { city } = getCity.data;
         // console.log({'city': city, 'country ': country})
 
         const apiUrl = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAccessToken}&units=metric`);
