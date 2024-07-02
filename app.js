fd170ca3c8d4372e724d67bba64107975fa3c750
Olubunmi <bjemiyor@gmail.com>
2024-07-02 07:57:47 +0100

    added openweather api key in app.js

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ const PORT = process.env.PORT || 3000;
 const dotenv = require('dotenv');  // Load dotenv package
 dotenv.config({ path: './config.env' });
 
+const weatherAccessToken = process.env.WEATHER_API_KEY
+
 // Middleware to parse JSON request body
 app.use(express.json());
 
@@ -48,6 +50,8 @@ app.get('/api/hello', async (req, res) => {
     let clientIp = req.ip;  // This gives you the IP address of the requester
     
     const accessToken = process.env.ACCESS_TOKEN;
+    
+    
     console.log('access: ', accessToken)
 
     // Normalize IPv4-mapped IPv6 address to IPv4 format (if needed)
@@ -57,15 +61,22 @@ app.get('/api/hello', async (req, res) => {
 
     try {
         // Fetch geolocation data based on IP address from ipinfo.io
-        const response = await axios.get(`https://ipinfo.io/${clientIp}/json?token=${accessToken}`);
+        // const response = await axios.get(`https://ipinfo.io/${clientIp}/json?token=${accessToken}`);
        
-        const { city } = response.data;  // Extract city from the response
+        // const { city } = response.data;  // Extract city from the response
 
-        // const { city, region, country } = response.data;
+        const getCity = await axios.get(`http://ip-api.com/json/?fields=61439`)
+        // console.log(getCity)
+
+        const { city, country } = getCity.data;
+        // console.log({'city': city, 'country ': country})
+
+        const apiUrl = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAccessToken}&units=metric`);
+        // console.log(apiUrl)
         
 
         // Prepare the response object
-        let greeting = `Hello, ${visitorName}!, the temperature is 11 degrees Celsius`;
+        let greeting = `Hello, ${visitorName}!, the temperature is ${apiUrl.data.main.temp} degrees Celsius`;
         if (city) {
             greeting += ` in ${city}`;
         }
@@ -77,7 +88,7 @@ app.get('/api/hello', async (req, res) => {
         };
 
         // Send the response as JSON
-        res.json(responseData);
+        res.status(200).json(responseData);
     } catch (error) {
         console.error('Error fetching IP geolocation:', error.message);
         res.status(500).json({ error: 'Failed to fetch IP geolocation' });
